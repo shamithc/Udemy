cbd2ec292851a3ea206a62e86c119414b93b4554
shamith <shamith.c@bridge-india.in>
2015-04-17 20:22:06 +0530

    Chapter-3

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter){
 	$scope.handle = ""
+  $scope.handlelength = 5;
 	
 	$scope.lowercaseHandle = function(){
 		return $filter('lowercase')($scope.handle);
 	};
 	
+  $scope.rules = [
+    {rulename: "First Rule!"},
+    {rulename: "Second Rule!"},
+    {rulename: "Third Rule!"}
+  ];
 
 }]);
