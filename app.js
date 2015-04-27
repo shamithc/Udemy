17709dee64056d0b9698b28678bcb6eed5306579
shamithc <shamithc@gmail.com>
2015-04-27 14:02:44 +0530

    Single page app

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter){
-	$scope.handle = ""
-  $scope.handlelength = 5;
-	
-	$scope.lowercaseHandle = function(){
-		return $filter('lowercase')($scope.handle);
-	};
-	
-  $scope.rules = [
-    {rulename: "First Rule!"},
-    {rulename: "Second Rule!"},
-    {rulename: "Third Rule!"}
-  ];
+myApp.config(function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: 'pages/main.html',
+		controller: 'mainController'
+	})
+	.when('/second', {
+		templateUrl: 'pages/second.html',
+		controller: 'secondController'
+	})
+	.when('/second/:num', {
+		templateUrl: 'pages/second.html',
+		controller: 'secondController'
+	})
+});
 
+myApp.service('nameService', function(){
+	var self = this;
+	self.name = "Jhon Woo";
+
+	this.nameLength = function(){
+		return self.name.length;
+	}
+});
+
+myApp.controller('mainController', ['$scope', 'nameService', function($scope, nameService){
+	console.log('Yes, I am here');
+	$scope.name = nameService.name;
+	$scope.$watch('name', function(newValue){
+		nameService.name = newValue;
+	});
+}]);
+
+myApp.controller('secondController', ['$scope', '$routeParams', 'nameService', function($scope, $routeParams, nameService){
+	$scope.name = nameService.name;
+	$scope.num = $routeParams.num || 0;
+	$scope.nameLength = nameService.nameLength();
 }]);
